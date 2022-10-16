6d20caa3b0d3d60f293307d9a41e7178563f23da
user <>
2022-10-17 00:32:31 +0300

    completed task

--- app/src/main/java/com/artemchep/basics_multithreading/MainActivity.java
+++ app/src/main/java/com/artemchep/basics_multithreading/MainActivity.java
@@ -1,31 +1,30 @@
 package com.artemchep.basics_multithreading;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-
 import androidx.annotation.UiThread;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.artemchep.basics_multithreading.cipher.CipherUtil;
 import com.artemchep.basics_multithreading.domain.Message;
 import com.artemchep.basics_multithreading.domain.WithMillis;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
-
-    private List<WithMillis<Message>> mList = new ArrayList<>();
-
-    private MessageAdapter mAdapter = new MessageAdapter(mList);
+public class MainActivity extends AppCompatActivity implements ThreadQueueInterface {
+    private final String TAG = "MainActivity";
+    private final List<WithMillis<Message>> mList = new ArrayList<>();
+    private final ThreadQueue threadQueue = new ThreadQueue();
+    private final MessageAdapter mAdapter = new MessageAdapter(mList);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        threadQueue.setCallBack(this);
 
         final RecyclerView recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -44,27 +43,38 @@ public class MainActivity extends AppCompatActivity {
                 .show();
     }
 
-    public void onPushBtnClick(View view) {
+    public void onPushBtnClick(View view) throws InterruptedException {
         Message message = Message.generate();
-        insert(new WithMillis<>(message));
+        long startTime = System.currentTimeMillis();
+        Log.d(TAG, "Current Time: " + startTime);
+        insert(new WithMillis<>(message, startTime));
     }
 
     @UiThread
-    public void insert(final WithMillis<Message> message) {
+    public void insert(final WithMillis<Message> message) throws InterruptedException {
         mList.add(message);
-        mAdapter.notifyItemInserted(mList.size() - 1);
+        mAdapter.notifyItemInserted(   mList.size() - 1);
 
         // TODO: Start processing the message (please use CipherUtil#encrypt(...)) here.
         //       After it has been processed, send it to the #update(...) method.
+
+        threadQueue.addItemToQueue(message);
+        Log.d(TAG, "Thread: " + Thread.currentThread().getName());
 
         // How it should look for the end user? Uncomment if you want to see. Please note that
         // you should not use poor decor view to send messages to UI thread.
 //        getWindow().getDecorView().postDelayed(new Runnable() {
 //            @Override
 //            public void run() {
-//                final Message messageNew = message.value.copy("sample :)");
-//                final WithMillis<Message> messageNewWithMillis = new WithMillis<>(messageNew, CipherUtil.WORK_MILLIS);
+//                final long time = System.currentTimeMillis();
+//
+//                final Message messageNew = message.value.copy(CipherUtil.encrypt(message.value.plainText));
+//                final WithMillis<Message> messageNewWithMillis = new WithMillis<>(messageNew, CipherUtil.WORK_MILLIS + (System.currentTimeMillis() - time));
 //                update(messageNewWithMillis);
+//                Log.d("Thread state is ", String.valueOf(Thread.currentThread().getState()));
+//                Log.d("Thread name is ", Thread.currentThread().getName());
+//                Log.d("Time spend in queue: ", String.valueOf(CipherUtil.WORK_MILLIS + (System.currentTimeMillis() - time)));
+//                Log.d("Thread name is ", Thread.currentThread().getName());
 //            }
 //        }, CipherUtil.WORK_MILLIS);
     }
@@ -82,4 +92,14 @@ public class MainActivity extends AppCompatActivity {
         throw new IllegalStateException();
     }
 
+    @Override
+    public void onListUpdated(final WithMillis<Message> messageWithMillis) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                update(messageWithMillis);
+                Log.d(TAG, "List updated in Thread: " + threadQueue.getName());
+            }
+        });
+    }
 }
---
+++ app/src/main/java/com/artemchep/basics_multithreading/ThreadQueue.java
@@ -0,0 +1,79 @@
+package com.artemchep.basics_multithreading;
+
+import android.util.Log;
+
+import com.artemchep.basics_multithreading.cipher.CipherUtil;
+import com.artemchep.basics_multithreading.domain.Message;
+import com.artemchep.basics_multithreading.domain.WithMillis;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+interface ThreadQueueInterface {
+    void onListUpdated(WithMillis<Message> messageWithMillis);
+}
+
+public class ThreadQueue extends Thread {
+    private final BlockingQueue<WithMillis<Message>> queue = new LinkedBlockingQueue<WithMillis<Message>>();
+    private Boolean isRunning = true;
+    private ThreadQueueInterface callBack;
+
+    ThreadQueue() {
+        this.start();
+    }
+
+    public void setCallBack(ThreadQueueInterface update) {
+        callBack = update;
+    }
+
+    @Override
+    public void run() {
+        while (isRunning) {
+            if (queue.isEmpty()) {
+                synchronized (this) {
+                    try {
+                        this.wait();
+                        Log.d("ThreadQueue:", currentThread().getName() + " is waiting");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else {
+                synchronized (queue) {
+                    Log.d("ThreadQueue:", "Thread name: " + currentThread().getName());
+
+                    WithMillis<Message> messageWithMillis = queue.poll();
+                    Message message = messageWithMillis.value;
+
+                    long startEncryptingTime = System.currentTimeMillis();
+                    final String encrypt = CipherUtil.encrypt(message.plainText);
+                    long endEncryptingTime = System.currentTimeMillis() - startEncryptingTime;
+                    long duration = messageWithMillis.elapsedMillis + System.currentTimeMillis() + endEncryptingTime;
+
+                    Message newMessage = message.copy(encrypt);
+                    final WithMillis<Message> newMessageWithMillis = new WithMillis<>(newMessage, duration);
+                    Log.d("ThreadQueue:", "Message: " + newMessage.cipherText);
+                    Log.d("ThreadQueue:", "WithMillis<Message>: " + newMessageWithMillis.elapsedMillis);
+
+                    callBack.onListUpdated(newMessageWithMillis);
+                }
+            }
+        }
+    }
+
+    public void addItemToQueue(WithMillis<Message> message) throws InterruptedException {
+        //add item to queue
+        synchronized (this) {
+            queue.put(message);
+            Log.d("ThreadQueue:", "Item added");
+            Log.d("ThreadQueue:", "Queue: " + queue);
+
+            //notify all threads about item added to queue
+            this.notifyAll();
+            Log.d("ThreadQueue:", "Thread: " + currentThread().getName() + " notifiying");
+            Log.d("ThreadQueue:", "Thread: " + currentThread().getState());
+        }
+    }
+
+
+}
