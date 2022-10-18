37c49cdd0462ae6ae1a67d26a1bac69b872b1823
user <>
2022-10-18 18:02:03 +0300

    revoved commented code

--- app/src/main/java/com/artemchep/basics_multithreading/ThreadQueue.java
+++ app/src/main/java/com/artemchep/basics_multithreading/ThreadQueue.java
@@ -31,46 +31,7 @@ public class ThreadQueue extends Thread {
     public void run() {
         while (isRunning) {
             Runnable encryptingTask = getRunnable();
-//            encryptingTask.run();
-//            Runnable encryptingTask = null;
-//            synchronized (this) {
-//                if (queue.isEmpty()) {
-//                    try {
-//                        Log.d("ASD", "prepare for wait");
-//                        if (isRunning) {
-//                            this.wait();
-//                        }
-//                    } catch (InterruptedException e) {
-//                        e.printStackTrace();
-//                    }
-//                } else {
-//                    final WithMillis<Message> messageWithMillis = queue.poll();
-//                    if (messageWithMillis != null) {
-//                        Runnable newTask = new Runnable() {
-//                            @Override
-//                            public void run() {
-//                                Log.d("ThreadQueue:", "Thread name: " + currentThread().getName());
-//
-//                                Message message = messageWithMillis.value;
-//
-//                                final String encrypt = CipherUtil.encrypt(message.plainText);
-//
-//                                Message newMessage = message.copy(encrypt);
-//                                long endEncryptingTime = SystemClock.elapsedRealtime();
-//                                long duration = endEncryptingTime - messageWithMillis.elapsedMillis;
-//
-//                                final WithMillis<Message> newMessageWithMillis = new WithMillis<>(newMessage, duration);
-//                                Log.d("ThreadQueue:", "Message: " + newMessage.cipherText);
-//                                Log.d("ThreadQueue:", "WithMillis<Message>: " + newMessageWithMillis.elapsedMillis);
-//                                Log.d("ThreadQueue:", "Duration: " + duration);
-//
-//                                callBack.onListUpdated(newMessageWithMillis);
-//                            }
-//                        };
-//                        encryptingTask = newTask;
-//                    }
-//                }
-//            }
+
             try {
                 encryptingTask.run();
                 Log.d("ThreadQueue:", currentThread().getName());
